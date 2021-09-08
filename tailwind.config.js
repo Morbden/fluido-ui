707816377ddc263ae551abc5dfe3449ac55d4b04
Renato Rodrigues <ren.gsg@gmail.com>
2021-09-07 21:07:34 -0300

    adição do config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './components/**/*.{js}',
+    './effects/**/*.{js}',
+    './hooks/**/*.{js}',
+    './layouts/**/*.{js}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: colors.emerald,
+        accent: colors.fuchsia,
+        canvas: colors.amber,
+        neutral: colors.gray,
+      },
+      fontFamily: {
+        display: ['Oswald', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
