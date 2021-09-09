dfa50fbf08b1e2dde2d66a381f05508d6bffb571
Renato Rodrigues <ren.gsg@gmail.com>
2021-09-09 13:15:48 -0300

    correção do webpack

--- src/settings/webpack-fix.ts
+++ src/settings/webpack-fix.ts
@@ -4,6 +4,9 @@ import { join } from 'path'
 export function webpackPresetConfig(config: Configuration) {
   const gooberAlias = {
     goober: [join(process.cwd(), 'node_modules/goober')],
+    react: [join(process.cwd(), 'node_modules/react')],
+    'react-dom': [join(process.cwd(), 'node_modules/react-dom')],
+    'framer-motion': [join(process.cwd(), 'node_modules/framer-motion')],
   }
 
   if (!config.resolve) {
