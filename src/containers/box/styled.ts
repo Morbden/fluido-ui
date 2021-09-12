de50220380dd81bfea2629f1efb95eca5dbdef8d
Gilson Nunes Filho <gilson@fluido.design>
2021-09-12 12:20:05 -0300

    documentation for base padding props

--- src/containers/box/styled.ts
+++ src/containers/box/styled.ts
@@ -2,19 +2,62 @@ import { styled } from 'ui-styled/styled'
 import { Length, StyledProps } from 'ui-types/styled'
 
 export interface BoxProps_ {
-  p?: Length
+  /** One, two, three or four values for CSS `padding` shorthand
+   * @default null
+   */
+  p?:
+    | Length
+    | [Length, Length]
+    | [Length, Length, Length]
+    | [Length, Length, Length, Length]
+  /** Single value for CSS `padding-top`
+   * @default null
+   */
   pt?: Length
+  /** Single value for CSS `padding-right`
+   * @default null
+   */
   pr?: Length
+  /** Single value for CSS `padding-bottom`
+   * @default null
+   */
   pb?: Length
+  /** Single value for CSS `padding-left`
+   * @default null
+   */
   pl?: Length
+  /** Single value for CSS `padding-top` plus `padding-bottom`
+   * @default null
+   */
   py?: Length
+  /** Single value for CSS `padding-left` plus `padding-right`
+   * @default null
+   */
   px?: Length
-  pil?: Length
-  pis?: Length
-  pie?: Length
+  /** One or two values for CSS `padding-block`. Shorthand for `padding-block-start` plus `padding-block-end`
+   * @default null
+   */
   pbl?: Length
+  /** Value for CSS `padding-block-start`
+   * @default null
+   */
   pbs?: Length
+  /** Value for CSS `padding-block-end`
+   * @default null
+   */
   pbe?: Length
+  /** One or two values for CSS `padding-inline`. Shorthand for `padding-inline-start` plus `padding-inline-end`
+   * @default null
+   */
+  pil?: Length | [Length, Length]
+  /** Value for CSS `padding-inline-start`
+   * @default null
+   */
+  pis?: Length
+  /** Value for CSS `padding-inline-end`
+   * @default null
+   */
+  pie?: Length
 }
 
 export const Box_ = styled('div')<BoxProps_ & StyledProps>`
