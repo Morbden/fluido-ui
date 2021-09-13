bbe1480efaa10a64edec197715e83cecb2400a3d
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 14:37:13 -0300

    Sidebar component

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { forwardRef } from 'react'
 import { FluiComponent } from 'ui-types'
+import { useTheme } from 'ui-contexts'
 import { SidebarProps_, Sidebar_ } from './styled'
 
 export interface SidebarProps extends SidebarProps_ {}
@@ -7,9 +8,15 @@ export interface SidebarProps extends SidebarProps_ {}
 export type SidebarType = FluiComponent<'div', SidebarProps>
 
 export const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, contentMin, gap, equalHeight, ...props }, ref) => {
+    const theme = useTheme()
     return (
-      <Sidebar_ ref={ref} {...props}>
+      <Sidebar_
+        ref={ref}
+        contentMin={contentMin || '50%'}
+        equalHeight={equalHeight || true}
+        gap={gap || theme.spaces.md}
+        {...props}>
         {children}
       </Sidebar_>
     )
--- src/containers/sidebar/styled.ts
+++ src/containers/sidebar/styled.ts
@@ -1,6 +1,41 @@
 import { Box, BoxProps } from '../box'
 import { styled } from 'ui-styled'
+import { Length } from 'ui-types'
 
-export interface SidebarProps_ extends BoxProps {}
+export interface SidebarProps_ extends BoxProps {
+  contentMin?: Length
+  equalHeight?: boolean
+  gap?: Length
+  noStretch?: boolean
+  side?: 'left' | 'right'
+  sideWidth?: Length
+}
 
-export const Sidebar_ = styled(Box)<SidebarProps_>``
+export const Sidebar_ = styled(Box)<SidebarProps_>`
+  display: flex;
+  flex-wrap: wrap;
+  gap: $gap;
+  align-items: $equalHeight;
+
+  & > :first-child {
+    flex-basis: $sideWidth;
+    flex-grow: 1;
+  }
+
+  & > :last-child {
+    flex-basis: 0;
+    flex-grow: 9999;
+    min-width: min($contentMin, 100%);
+  }
+
+  /* & > $if(side = left, :first-child, :last-child) {
+    flex-basis: $sideWidth;
+    flex-grow: 1;
+  }
+
+  & > $if(side = left, :last-child, :first-child) {
+    flex-basis: 0;
+    flex-grow: 999;
+    min-width: $contentMin;
+  } */
+`
