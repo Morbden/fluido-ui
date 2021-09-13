f0b98cc052837a0949523ad463b57bb26c33aa21
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 18:50:39 -0300

    remove icon and imposter containers

--- src/containers/icon/styled.ts
+++
@@ -1,6 +0,0 @@
-import { Box, BoxProps } from '../box'
-import { styled } from 'ui-styled'
-
-export interface IconProps_ extends BoxProps {}
-
-export const Icon_ = styled(Box)<IconProps_>``
--- src/containers/imposter/styled.ts
+++
@@ -1,6 +0,0 @@
-import { Box, BoxProps } from '../box'
-import { styled } from 'ui-styled'
-
-export interface ImposterProps_ extends BoxProps {}
-
-export const Imposter_ = styled(Box)<ImposterProps_>``
