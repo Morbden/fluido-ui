223fce3703409e8a137223820d9e25a889777863
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 14:36:17 -0300

    cluster component

--- src/containers/cluster/index.tsx
+++ src/containers/cluster/index.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from 'ui-contexts'
 import { forwardRef } from 'react'
 import { FluiComponent } from 'ui-types'
 import { ClusterProps_, Cluster_ } from './styled'
@@ -7,9 +8,10 @@ export interface ClusterProps extends ClusterProps_ {}
 export type ClusterType = FluiComponent<'div', ClusterProps>
 
 export const Cluster = forwardRef<HTMLDivElement, ClusterProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, gap, ...props }, ref) => {
+    const theme = useTheme()
     return (
-      <Cluster_ ref={ref} {...props}>
+      <Cluster_ gap={gap || theme.spaces.md} ref={ref} {...props}>
         {children}
       </Cluster_>
     )
--- src/containers/cluster/styled.ts
+++ src/containers/cluster/styled.ts
@@ -1,6 +1,23 @@
 import { Box, BoxProps } from '../box'
 import { styled } from 'ui-styled'
+import { Length } from 'ui-types'
 
-export interface ClusterProps_ extends BoxProps {}
+export interface ClusterProps_ extends BoxProps {
+  gap?: Length
+  justify?:
+    | 'center'
+    | 'flex-end'
+    | 'flex-start'
+    | 'space-around'
+    | 'space-between'
+    | 'space-evenly'
+  items?: 'baseline' | 'center' | 'flex-end' | 'flex-start' | 'stretch'
+}
 
-export const Cluster_ = styled(Box)<ClusterProps_>``
+export const Cluster_ = styled(Box)<ClusterProps_>`
+  display: flex;
+  flex-wrap: wrap;
+  gap: $gap;
+  justify-content: $justify;
+  align-items: $items;
+`
