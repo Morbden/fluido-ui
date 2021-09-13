756b634ad90dd771b8553832a83ef6a6d9d8e5ff
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 18:51:06 -0300

    frame testing nested functions

--- src/containers/frame/styled.ts
+++ src/containers/frame/styled.ts
@@ -1,6 +1,58 @@
 import { Box, BoxProps } from '../box'
 import { styled } from 'ui-styled'
 
-export interface FrameProps_ extends BoxProps {}
+export interface FrameProps_ extends BoxProps {
+  allowOverflow?: boolean
+  context?: boolean
+  objectFit?: 'cover' | 'contain'
+  preserveRatio?: boolean
+  ratio?: string | number
+}
 
-export const Frame_ = styled(Box)<FrameProps_>``
+function getRatios(ratio: string | number) {
+  if (typeof ratio === 'string') {
+    const ratios = ratio.trim().split(/\/|:|x/)
+    if (ratios.length === 1) ratios[1] = ratios[0]
+    return `
+    --aspect-w: ${ratios[0]};
+    --aspect-h: ${ratios[1]};
+    `
+  }
+  return `
+    --aspect-w: ${ratio};
+    --aspect-h: ${1};
+    `
+}
+
+export const Frame_ = styled(Box)<FrameProps_>`
+  ${({ ratio }) => getRatios(ratio || 1)}
+  aspect-ratio: var(--aspect-w) / var(--aspect-h);
+  display: grid;
+  grid-template-areas: 'cell';
+  position: relative;
+  overflow: #if(!$allowOverflow, hidden);
+  min-height: #if($preserveRatio, 0);
+  isolation: #if($context, isolate);
+
+  & > img,
+  & > video,
+  & > [style] {
+    width: 100%;
+    height: 100%;
+    object-fit: $objectFit;
+  }
+
+  @supports not (aspect-ratio: 1 / 1) {
+    &::before {
+      grid-area: cell;
+      padding-bottom: calc(var(--aspect-h) / var(--aspect-w) * 100%);
+      content: '';
+    }
+
+    & > * {
+      grid-area: cell;
+      position: #if($allowOverflow, absolute);
+      position: #if($preserveRatio, absolute);
+    }
+  }
+`
