fddfc57b2bef5175f45ff1717181a498c2f7e806
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 18:51:30 -0300

    switch container

--- src/containers/switcher/index.tsx
+++ src/containers/switcher/index.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react'
+import { useTheme } from 'ui-contexts'
 import { FluiComponent } from 'ui-types'
 import { SwitcherProps_, Switcher_ } from './styled'
 
@@ -7,9 +8,15 @@ export interface SwitcherProps extends SwitcherProps_ {}
 export type SwitcherType = FluiComponent<'div', SwitcherProps>
 
 export const Switcher = forwardRef<HTMLDivElement, SwitcherProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, gap, limit, threshold, ...props }, ref) => {
+    const theme = useTheme()
     return (
-      <Switcher_ ref={ref} {...props}>
+      <Switcher_
+        gap={gap || theme.spaces.xl}
+        limit={limit || 4}
+        threshold={threshold || 120}
+        ref={ref}
+        {...props}>
         {children}
       </Switcher_>
     )
--- src/containers/switcher/styled.ts
+++ src/containers/switcher/styled.ts
@@ -1,6 +1,29 @@
 import { Box, BoxProps } from '../box'
 import { styled } from 'ui-styled'
+import { Length } from 'ui-types'
 
-export interface SwitcherProps_ extends BoxProps {}
+export interface SwitcherProps_ extends BoxProps {
+  threshold?: Length
+  gap?: Length
+  limit?: number
+}
 
-export const Switcher_ = styled(Box)<SwitcherProps_>``
+export const Switcher_ = styled(Box)<SwitcherProps_>`
+  display: flex;
+  flex-wrap: wrap;
+  gap: $gap;
+
+  & > * {
+    flex-grow: 1;
+    flex-basis: calc(($threshold - 100%) * 999);
+  }
+
+  & > :nth-last-child(n + ${({ limit }) => (limit ? limit + 1 : null)}),
+  & > :nth-last-child(n + ${({ limit }) => (limit ? limit + 1 : null)}) ~ * {
+    flex-basis: 100%;
+  }
+
+  & > * > * {
+    max-width: 100%;
+  }
+`
