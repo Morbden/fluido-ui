5dcc7aadad06753d58942d85fbd7030a668997ee
Gilson Nunes Filho <gilson@fluido.design>
2021-09-13 14:37:29 -0300

    Stack update

--- src/containers/stack/styled.ts
+++ src/containers/stack/styled.ts
@@ -4,13 +4,24 @@ import { Length } from 'ui-types'
 
 export interface StackProps_ extends BoxProps {
   gap?: Length
+  splitAfter?: number | number[]
+}
+
+function makeSplitSelector(value: number | number[]) {
+  if (Array.isArray(value)) {
+    return value.map((v) => `:nth-child(${v + 1})`).join(',')
+  }
+  return `:nth-child(${value + 1})`
 }
 
 export const Stack_ = styled(Box)<StackProps_>`
-  outline: 1px solid red;
-  & > * {
-    outline: 1px solid green;
+  display: flex;
+  flex-direction: column;
+
+  & > :is(${({ splitAfter }) => splitAfter && makeSplitSelector(splitAfter)}) {
+    margin-top: auto;
   }
+
   & > * + * {
     margin-top: $gap;
   }
